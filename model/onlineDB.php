77ad3f499eeff9b42f1089147d5dc7635f630778
Tariq Yousef <info@tariq-yousef.com>
2016-01-31 19:09:42 +0100

    Update onlineDB.php

--- model/onlineDB.php
+++ model/onlineDB.php
@@ -1,9 +1,9 @@
 <?
 // Online DB
-$host="db597922715.db.1and1.com";
-$Dbname="db597922715";
-$username="dbo597922715";
-$password="Tariq@2015";
+$host="*****************";
+$Dbname="*****************";
+$username="*****************";
+$password="*****************";
 $con= new mysqli($host, $username, $password, $Dbname);
 $con->set_charset("utf8");
 
